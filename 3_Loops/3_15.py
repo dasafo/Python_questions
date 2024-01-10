09702fab6bd005ae197469d81a44c74abc92bddc
dsf <dasafo1985@gmail.com>
2024-01-10 15:54:49 +0100

    100124

--- 3_Loops/3_15.py
+++ 3_Loops/3_15.py
@@ -1,4 +1,15 @@
 # Use a loop to display elements from a given 
 # list present at odd index positions
 
+list = [45, 6, 43, 2, 89, 76, 22]
 
+# Method 1
+for i in range(0, len(list)):
+    if i % 2 !=0:
+        print(list[i], end=" ")
+
+print()
+
+# Method 2
+for i in list[1::2]:
+    print(i, end=" ")
---
+++ 3_Loops/3_16.py
@@ -0,0 +1,7 @@
+# Calculate the cube of all numbers from 1 to 
+# a given number
+
+# Write a program to rint the cube of all 
+# numbers from 1 to a given number
+
+
