864417f9a0f99b5245d7eb0c3f8154fc73cfbf2d
dasafo <dasafo1985@gmail.com>
2024-08-20 17:11:36 +0200

    200824

--- 5_String/5_4.py
+++ 5_String/5_4.py
@@ -1,6 +1,6 @@
 # Arrange string characters such that lowercase letters should come first
 
-# Given string contains a combination of the lower and
+# Given string conta a combination of the lower and
 # upper case letters. Write a program to arrange the
 # characters of a string so that all lowercase letters should come first.
 
